082b72d5308acb2a4d1df3aedf1db1a667be979e
osipover <104246010+osipover@users.noreply.github.com>
2022-07-28 18:40:46 +0700

    Create main.c

---
+++ HOMEWORKS/ПОБИТОВЫЕ ОПЕРАЦИИ/Умножение через побитовые сдвиги/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <assert.h>
+
+void GenerateMultByConst(unsigned int multiplier) {
+	static int position = 0;
+
+	if (position == 0)
+		printf("int MultBy%u(unsigned int x){\n\treturn ", multiplier);
+
+	if ((multiplier == 0) && (position == 0)) {
+		printf("0;\n}");
+		return;
+	}
+
+	if ((multiplier & (1 << position)) != 0) {
+		printf("(x << %d)", position);
+
+		unsigned char mask = ~(1 << position);
+
+		if ((multiplier & mask) != 0) {
+			printf(" + ");
+			multiplier &= mask;
+		}
+		else {
+			printf(";\n}");
+			return;
+		}
+	}
+
+	++position;
+	GenerateMultByConst(multiplier);
+}
+
+int main() {
+	unsigned int multiplier;
+	assert(scanf("%u", &multiplier) == 1);
+	GenerateMultByConst(multiplier);
+}
